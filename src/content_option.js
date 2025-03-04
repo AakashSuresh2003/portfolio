5ff9e73a5435bcc31d8dbdf660a2d49fbe51e89f
Aakash <aakashsuresh455@gmail.com>
2025-03-04 20:22:48 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,134 @@
+const logotext = "Aakash Suresh";
+const meta = {
+    title: "Aakash Suresh",
+    description: "I am Aakash Suresh, a Full Stack Developer and DevOps Engineer currently based in Chennai. With experience in developing end-to-end solutions, I specialize in both front-end and back-end technologies. I focus on streamlining development processes and improving system efficiency, bridging the gap between application development and operational excellence.",
+};
+
+const introdata = {
+    title: "I’m Aakash Suresh",
+    animated: {
+        first: "Building innovative solutions with code.",
+        second: "Transforming ideas into seamless digital experiences.",
+        third: "Passionate about optimizing both development and operations.",
+    },
+    description: "I am Aakash Suresh, a Full Stack Developer and DevOps Engineer currently based in Chennai. With experience in developing end-to-end solutions, I specialize in both front-end and back-end technologies. I focus on streamlining development processes and improving system efficiency, bridging the gap between application development and operational excellence.",
+    your_img_url: "..//my_image.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Monolith Research and Training Labs Pvt Ltd.",
+        where: "Chennai",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "C#",
+        value: 90,
+    },
+    {
+        name: "Express.js",
+        value: 98,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Typescript",
+        value: 85,
+    },
+    {
+        name: "Jest",
+        value: 85,
+    },
+    {
+        name: ".Net Core",
+        value: 85,
+    },
+];
+
+const services = [{
+    title: "Front End Development",
+    description: "I build dynamic and responsive user interfaces using modern web technologies like React, HTML, CSS, and JavaScript to create seamless and interactive user experiences.",
+},
+{
+    title: "Back End Development",
+    description: "I develop robust server-side applications and databases, utilizing technologies like Node.js, Express, and databases like MongoDB, to build scalable and efficient systems.",
+},
+{
+    title: "DevOps Engineering",
+    description: "I automate and streamline the deployment pipeline, manage infrastructure, and ensure system reliability using tools like Docker, Kubernetes, CI/CD, and cloud platforms.",
+},
+];
+
+const dataportfolio = [{
+    img: "../react.svg", // Official React logo
+    description: "Built a dynamic web application using React for the front end and Express.js for the back end. Leveraged Docker for containerization.",
+    link: "#",
+},
+{
+    img: "../jenkins.png", // Official Jenkins logo
+    description: "Automated CI/CD pipeline using Jenkins to ensure seamless code integration and deployment. Integrated Docker and Terraform for infrastructure automation.",
+    link: "#",
+},
+{
+    img: "../ansible.jpg", // Official Ansible logo
+    description: "Used Ansible to automate server configurations and deployments, ensuring consistent environments across multiple machines.",
+    link: "#",
+},
+{
+    img: "../azure.svg", // Official AWS logo
+    description: "Deployed applications on **Azure** for scalable cloud infrastructure, with Terraform managing the infrastructure as code.",
+    link: "#",
+},
+{
+    img: "../terraform.webp", // Official Terraform logo
+    description: "Automated infrastructure provisioning using **Terraform** to manage cloud resources efficiently.",
+    link: "#",
+},
+{
+    img: "../github.webp", // Official Nexus logo
+    description: "Leveraged **GitHub** for version control and collaboration on software development projects, ensuring efficient workflow and code review.",
+    link: "#",
+},
+{
+    img: "../express.png", // Official Express.js logo
+    description: "Developed a full-stack application using **React** for the frontend and **Express.js** for the backend, focusing on performance and scalability.",
+    link: "#",
+}
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aakashakshith455@gmail.com",
+    YOUR_FONE: "+91 8667698884",
+    description: "I’d love to hear from you! Whether you have a question, need assistance, or simply want to say hello, feel free to reach out. I'm always ready to assist with any inquiries or opportunities. You can contact me through the form below or directly via email. I’ll get back to you as soon as possible.",
+    YOUR_SERVICE_ID: "service_p9b001u",
+    YOUR_TEMPLATE_ID: "template_ojhxz0i",
+    YOUR_USER_ID: "U_s0i7LdDwtPAUrah",
+};
+
+const socialprofils = {
+    github: "https://github.com/AakashSuresh2003",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
